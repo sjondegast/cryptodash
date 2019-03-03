e7d39b03a6234ed560862fe2f725caff0a29c765
sjon de gast <>
2019-03-03 17:22:42 +0100

    changed App foldername to Components and cleaned up a little

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+
+
+class App extends Component {
+  render() {
+    return (
+      <WelcomeMessage/>
+    );
+  }
+}
+
+export default App;
