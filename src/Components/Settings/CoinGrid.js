54351cb4547de137a0e0333670d3208917cec564
sjon de gast <>
2019-03-06 13:31:11 +0100

    added coingrid and grid-template-columns style

--- src/Components/Settings/CoinGrid.js
+++ src/Components/Settings/CoinGrid.js
@@ -3,15 +3,18 @@ import styled, {css} from 'styled-components';
 import { AppContext } from '../AppProvider';
 
 export const CoinGridStyled = styled.div`
-	display: grid;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
 `;
 
 const CoinGrid = () => {
-	return <AppContext.Consumer>
+	return (
+        <AppContext.Consumer>
             {({coinList}) => <CoinGridStyled>
-                {Object.keys(coinList).length}
+                {Object.keys(coinList).map(coinKey => <div>{coinKey}</div>)}
                 </CoinGridStyled>}
 		</AppContext.Consumer>
+    );
 }
 
 export default CoinGrid;
