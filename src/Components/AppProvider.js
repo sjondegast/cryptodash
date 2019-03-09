ccdc68b1bd5f9678ddc54c7dc8e42b864ec7a4bc
sjon de gast <>
2019-03-09 20:03:46 +0100

    Fetch prices data

--- src/Components/AppProvider.js
+++ src/Components/AppProvider.js
@@ -11,70 +11,98 @@ export class AppProvider extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-            page: 'Dashboard',
-            favorites: ['BTC', 'ETH', 'XMR', 'DOGE'],
-            ...this.savedSettings(),
-            setPage: this.setPage,
-            addCoin: this.addCoin,
-            removeCoin: this.removeCoin,
-            isInFavorites: this.isInFavorites,
-            confirmFavorites: this.confirmFavorites,
-            setFilteredCoins: this.setFilteredCoins
+			page: 'Dashboard',
+			favorites: ['BTC', 'ETH', 'XMR', 'DOGE'],
+			...this.savedSettings(),
+			setPage: this.setPage,
+			addCoin: this.addCoin,
+			removeCoin: this.removeCoin,
+			isInFavorites: this.isInFavorites,
+			confirmFavorites: this.confirmFavorites,
+			setFilteredCoins: this.setFilteredCoins
 		};
-    }
+	}
 
-    componentDidMount = () => {
-        this.fetchCoins();
-    }
+	componentDidMount = () => {
+		this.fetchCoins();
+		this.fetchPrices();
+	};
 
-    fetchCoins = async () => {
-        let coinList = (await cc.coinList()).Data;
-        this.setState({coinList});
-    }
+	fetchCoins = async () => {
+		let coinList = (await cc.coinList()).Data;
+		this.setState({ coinList });
+	};
 
-    addCoin = key => {
-        let favorites = [...this.state.favorites];
-        if (favorites.length < MAX_FAVORITES) {
-            favorites.push(key);
-            this.setState({favorites});
-        }
-    }
+	fetchPrices = async () => {
+		if (this.state.firstVisit) return;
+		let prices = await this.prices();
+		console.log(prices);
+		this.setState({ prices });
+	};
 
-    removeCoin = key => {
-        let favorites = [...this.state.favorites];
-        this.setState({favorites: _.pull(favorites, key)});
+	prices = async () => {
+		let returnData = [];
+		for (let i = 0; i < this.state.favorites.length; i++) {
+			try {
+				let priceData = await cc.priceFull(this.state.favorites[i], 'USD');
+				returnData.push(priceData);
+			} catch (e) {
+				console.warn('Fetch price error: ', e);
+			}
+		}
+		return returnData;
+	};
 
-    }
+	addCoin = key => {
+		let favorites = [...this.state.favorites];
+		if (favorites.length < MAX_FAVORITES) {
+			favorites.push(key);
+			this.setState({ favorites });
+		}
+	};
 
-    isInFavorites = key => _.includes(this.state.favorites, key);
+	removeCoin = key => {
+		let favorites = [...this.state.favorites];
+		this.setState({ favorites: _.pull(favorites, key) });
+	};
 
-    confirmFavorites = () => {
-        this.setState({
-            firstVisit: false,
-            page: 'Dashboard'
-        });
-        localStorage.setItem('cryptoDash', JSON.stringify({
-            favorites: this.state.favorites
-        }));
-    }
+	isInFavorites = key => _.includes(this.state.favorites, key);
 
-    savedSettings() {
-        let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
-        if(!cryptoDashData) {
-            return { page: 'Settings', firstVisit: true}
-        }
-        let {favorites} = cryptoDashData;
-        return {favorites};
-    }
-    setPage = page => this.setState({ page });
+	confirmFavorites = () => {
+		this.setState(
+			{
+				firstVisit: false,
+				page: 'Dashboard'
+			},
+			() => {
+				this.fetchPrices();
+			}
+		);
+		localStorage.setItem(
+			'cryptoDash',
+			JSON.stringify({
+				favorites: this.state.favorites
+			})
+		);
+	};
 
-    setFilteredCoins = (filteredCoins) => this.setState({filteredCoins});
-    
-    render(){
-        return (
-            <AppContext.Provider value={this.state}>
-                {this.props.children}
-            </AppContext.Provider>
-        )
-    }
+	savedSettings() {
+		let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
+		if (!cryptoDashData) {
+			return { page: 'Settings', firstVisit: true };
+		}
+		let { favorites } = cryptoDashData;
+		return { favorites };
+	}
+	setPage = page => this.setState({ page });
+
+	setFilteredCoins = filteredCoins => this.setState({ filteredCoins });
+
+	render() {
+		return (
+			<AppContext.Provider value={this.state}>
+				{this.props.children}
+			</AppContext.Provider>
+		);
+	}
 }
