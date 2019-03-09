7199c0204ead1ae417c3e9e8376faf51553de41a
sjon de gast <>
2019-03-09 19:45:25 +0100

    added fuzzy search and display search results to tileGrid

--- src/Components/Settings/CoinGrid.js
+++ src/Components/Settings/CoinGrid.js
@@ -7,21 +7,33 @@ import CoinTile from './CoinTile';
 export const CoinGridStyled = styled.div`
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
-    grid-gap: 15px;
-    margin-top: 40px;
+	grid-gap: 15px;
+	margin-top: 40px;
 `;
 
-const getCoinsToDisplay = (coinList, topSection, favorites) => {
-	return topSection ? favorites : Object.keys(coinList).slice(10, 100);
+const getLowerSectionCoins = (coinList, filteredCoins) => {
+	return (
+		(filteredCoins && Object.keys(filteredCoins)) ||
+		Object.keys(coinList).slice(10, 100)
+	);
+};
+
+const getCoinsToDisplay = (coinList, topSection, favorites, filteredCoins) => {
+	return topSection ? favorites : getLowerSectionCoins(coinList, filteredCoins);
 };
 
 const CoinGrid = ({ topSection }) => {
 	return (
 		<AppContext.Consumer>
-			{({ coinList, favorites }) => (
+			{({ coinList, favorites, filteredCoins }) => (
 				<CoinGridStyled>
-					{getCoinsToDisplay(coinList, topSection, favorites).map(coinKey => (
-						<CoinTile topSection={topSection} coinKey={coinKey} key={coinKey}/>
+					{getCoinsToDisplay(
+						coinList,
+						topSection,
+						favorites,
+						filteredCoins
+					).map(coinKey => (
+						<CoinTile topSection={topSection} coinKey={coinKey} key={coinKey} />
 					))}
 				</CoinGridStyled>
 			)}
--- src/Components/Settings/Search.js
+++ src/Components/Settings/Search.js
@@ -1,26 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
-import { backgroundColor2, fontSize2} from '../Shared/Styles';
+import { backgroundColor2, fontSize2 } from '../Shared/Styles';
+import { AppContext } from '../AppProvider';
+import _ from 'lodash';
+import fuzzy from 'fuzzy';
 
 const SearchGrid = styled.div`
-    display: grid;
-    grid-template-columns: 200px 1fr;
+	display: grid;
+	grid-template-columns: 200px 1fr;
 `;
 
 const SearchInput = styled.input`
-    ${backgroundColor2}
-    ${fontSize2}
+	${backgroundColor2}
+	${fontSize2}
     border: 1px solid;
-    height: 25px;
-    color: #1163c9;
-    place-self: center left;
+	height: 25px;
+	color: #1163c9;
+	place-self: center left;
 `;
 
+const handleFilter = _.debounce((inputValue, coinList, setFilteredCoins) => {
+	// Get all the coin symbols
+	let coinSymbols = Object.keys(coinList);
+	// Get all the coin names, map symbol to name
+	let coinNames = coinSymbols.map(sym => coinList[sym].CoinName);
+	let allStringsToSearch = coinSymbols.concat(coinNames);
+	let searchResults = fuzzy
+		.filter(inputValue, allStringsToSearch, {})
+		.map(result => result.string);
+
+	let filteredCoins = _.pickBy(coinList, (result, symKey) => {
+		let coinName = result.CoinName;
+		return (
+			_.includes(searchResults, symKey) || _.includes(searchResults, coinName)
+		);
+    });
+    setFilteredCoins(filteredCoins);
+}, 500);
+
+const filterCoins = (e, setFilteredCoins, coinList) => {
+    let inputValue = e.target.value;
+    if(!inputValue) {
+        setFilteredCoins(null);
+        return;
+    }
+	handleFilter(inputValue, coinList, setFilteredCoins);
+};
+
 export default () => {
-    return (
-        <SearchGrid>
-            <h2>Search all coins</h2>
-            <SearchInput />
-        </SearchGrid>
-    );
-}
+	return (
+		<AppContext.Consumer>
+			{({ setFilteredCoins, coinList }) => (
+				<SearchGrid>
+					<h2>Search all coins</h2>
+					<SearchInput
+						onKeyUp={e => filterCoins(e, setFilteredCoins, coinList)}
+					/>
+				</SearchGrid>
+			)}
+		</AppContext.Consumer>
+	);
+};
