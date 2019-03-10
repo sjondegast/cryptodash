21a70f304608b327c614e7b83fc00161e142cc06
sjon de gast <>
2019-03-10 15:45:05 +0100

    Dashboard grid

---
+++ src/Components/Dashboard/PriceGrid.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import { AppContext } from '../AppProvider';
+import PriceTile from './PriceTile';
+
+const PriceGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-gap: 15px;
+    margin-top: 40px;
+`;
+
+export default () => {
+    return (
+        <AppContext.Consumer>
+            {({prices}) => (
+                <PriceGrid>
+                    {prices.map((price, index) => (
+                        <PriceTile price={price} index={index}/>
+                    ))}
+                </PriceGrid>
+            )}
+        </AppContext.Consumer>
+    )
+}
