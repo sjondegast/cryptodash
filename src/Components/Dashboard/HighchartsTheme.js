57ee508dc1e961f9e7ad1ff12fafe6e93cf22b3d
sjon de gast <>
2019-03-12 17:10:32 +0100

    added light theme

--- src/Components/Dashboard/HighchartsTheme.js
+++ src/Components/Dashboard/HighchartsTheme.js
@@ -1,13 +1,15 @@
+import { lightTheme } from '../Shared/Styles';
+
 const theme = {
 
     colors: ['#61d936', '#552ccb', '#1163c9', '#04a1ee', '#08c6e0',
         '#146b9e', '#f376c1', '#1b2839'],
     chart: {
-        backgroundColor: '#061a44',
+        backgroundColor: lightTheme ? 'white' : '#061a44',
         borderColor: '#000000',
         borderWidth: 0,
         className: 'dark-container',
-        plotBackgroundColor: '#061a44',
+        plotBackgroundColor: lightTheme ? 'white' : '#061a44',
         plotBorderWidth: 0
     },
     title: {
